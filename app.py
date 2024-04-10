d679148fd2efceed3ffcb71924ca8fbb90f33ad8
livia sydrião <liviasydriao@gmail.com>
2024-04-10 11:22:38 -0300

    adicionei contato pessoal 5

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ contato1 = ContatoProfissional('Livia','85999773136','empresa A','Analista')
 contato2 = ContatoPessoal('Saulo','85999773026','26/05/1992')
 contato3 = ContatoProfissional('Mel','85999773027','empresa B','Gerente')
 contato4 = ContatoProfissional('Leticia','85999773028','empresa C','Servidor')
+contato5 = ContatoPessoal('Mariana','85999773030','15/10/1985')
 
 agenda.adicionar_contato(contato1)
 agenda.adicionar_contato(contato2)
 agenda.adicionar_contato(contato3)
 agenda.adicionar_contato(contato4)
+agenda.adicionar_contato(contato5)
 
 agenda.exibir_info()
